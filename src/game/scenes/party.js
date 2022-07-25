eb68d454b3e17837880133bece3da64e4d5ee782
dragonjet <jetriconew@gmail.com>
2022-07-25 00:35:49 -0700

    Version bump, re-enable PWA

--- src/game/scenes/party.js
+++ src/game/scenes/party.js
@@ -37,7 +37,7 @@ class PartyScene extends Phaser.Scene {
         this.game.vue.dialog = true;
         this.game.vue.openProject = 'credits';
       });
-    this.add.text(5, 5, 'Version 220724.0235', {
+    this.add.text(5, 5, 'Version 220725.0034', {
       fontFamily: 'Arial',
       fontSize: 14,
       align: 'left',
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import { VueMasonryPlugin } from 'vue-masonry';
 import App from './App.vue';
-// import './registerServiceWorker';
+import './registerServiceWorker';
 import vuetify from './plugins/vuetify';
 import './plugins/mobile';
 
