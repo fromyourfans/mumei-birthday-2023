3d932534de792c427c7dfea0cbd90698ce6fa744
jetrico <jetriconew@gmail.com>
2023-07-28 18:35:00 +0800

    2023: certificate slideshow

--- src/components/ProjectCertificate.vue
+++ src/components/ProjectCertificate.vue
@@ -12,16 +12,36 @@
       </v-btn>
     </div>
     <div class="project-content" ref="container">
-      ...
+      <v-carousel height="100%" hide-delimiters progress>
+        <v-carousel-item :src="Cookbook01" contain ></v-carousel-item>
+        <v-carousel-item :src="Cookbook02" contain ></v-carousel-item>
+        <v-carousel-item :src="Cookbook03" contain ></v-carousel-item>
+        <v-carousel-item :src="Cookbook04" contain ></v-carousel-item>
+        <v-carousel-item :src="Cookbook05" contain ></v-carousel-item>
+        <v-carousel-item :src="Cookbook06" contain ></v-carousel-item>
+      </v-carousel>
     </div>
   </div>
 </template>
 
 <script>
+import Cookbook01 from '@/assets/mural-20220719.png';
+import Cookbook02 from '@/assets/mural-20220719.png';
+import Cookbook03 from '@/assets/mural-20220719.png';
+import Cookbook04 from '@/assets/mural-20220719.png';
+import Cookbook05 from '@/assets/mural-20220719.png';
+import Cookbook06 from '@/assets/mural-20220719.png';
+
 export default {
   props: ['image'],
   data: () => ({
     maxHeight: 480,
+    Cookbook01,
+    Cookbook02,
+    Cookbook03,
+    Cookbook04,
+    Cookbook05,
+    Cookbook06,
   }),
   methods: {
     onImgLoaded() {
