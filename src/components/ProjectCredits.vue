59603869c41e948925cb8f804fc37d532ed1a20a
jetrico <jetriconew@gmail.com>
2023-07-28 18:30:19 +0800

    2023: credits

--- src/components/ProjectCredits.vue
+++ src/components/ProjectCredits.vue
@@ -6,67 +6,65 @@
     <div class="project-content">
       <h3 class="brown--text">The Afterword</h3>
       <p>
-        Happy Birthday Mumshi!
-        This time alot of hoomans worked together to prepare a gift for you.
-        We put together a birthday party with many gifts for you to open!
-        As you can see, council and some hoomans are in attedance,
-        but know that everyone celebrates this special day with you,
-        including Myth, all Hololive Production talents, staff,
-        and 700,000 hoomans who truly like you!
+        Happy Birthday Mumei, Meimei, Mumeishi, Moomsuke, Moomei, Mooms, Moomers, etc!
+        This year the Hoomans have once again worked together
+        to create a website for your birthday gift!
+        There's lots of presents contained within here that were made with lots of
+        love and care in the hopes that we can repay back all the love and care you've given us.
+        Don't worry about being able to show everything on stream,
+        feel free to appreciate what you can and check it out later if you'd like!
       </p>
       <p>
-        The first and foremost "thank you" is for Nanashi Mumei.
-        You are the one who brought us together,
-        you are the one making us smile, laugh, and inspired regularly.
-        The hooman community was built around you, and the amazing things you show us.
-        In this special day, let us be the one giving back to you.
-      </p>
-      <p>
-        This is the day when we appreciate that Nanashi Mumei graced our world.
-        This day may have added 1 to your age, but isn't it a small price
-        to pay for the next whole year of fun moments we'll enjoy together?
+        A thousand thanks for being the one who brought us all together almost 2 years ago!
+        Without you, this project would never have been possible with you inspiring us
+        or making us laugh and smile every day.
+        Hopefully today, you'll let us be the one who can make you smile with our gift.
       </p>
 
       <h3 class="brown--text">Birthday Website</h3>
       <ul class="pb-4">
         <li><strong>Mumei's Civilization Discord Staff and Members</strong></li>
         <li><strong>Mumei's Sanctuary Discord Staff and Members</strong></li>
-        <li><strong>jetrico</strong> - Project Organizer, Lead Programmer, The Afterword</li>
+        <li><strong>jetrico</strong> - Site Project Organiser, Lead Programmer</li>
         <li><strong>Nin-tan【人単】</strong> - Room Artwork, Object Guides</li>
+        <li><strong>The Gnome's Prophet</strong> - Site Project Organiser, The Afterword</li>
+        <li><strong>Jackie Chan</strong> - Site Project Organiser, Video Project Organiser</li>
+        <li><strong>Mudrock</strong> - Site Project Organiser</li>
         <li>
-          Artworks
+          Assets
           <ul>
-            <li>Mumei - <strong>jetrico</strong></li>
-            <li>Sana - <strong>IanCSamson</strong></li>
-            <li>Fauna - <strong>faerieko</strong></li>
-            <li>Kronii - <strong>Abstraction</strong></li>
-            <li>Baelz - <strong>AlphaCas</strong></li>
+            <li>Mumei - <strong>Faerieko</strong></li>
+            <li>Sana - <strong>Pet</strong></li>
+            <li>Fauna - <strong>DeezNiceDrawins</strong></li>
+            <li>Kronii - <strong>Akkaネン</strong></li>
+            <li>Baelz - <strong>Sayowl</strong></li>
+            <li>Irys - <strong>Nin-tan【人単】</strong></li>
             <li>Room Layout - <strong>Nin-tan【人単】</strong></li>
-            <li>Birthday Cake - <strong>bukiyos_art</strong></li>
-            <li>Central Table - <strong>jetrico</strong></li>
-            <li>Birthday Banner - <strong>Trix Mix</strong></li>
-            <li>Hooman Avatars - <strong>collywop5</strong></li>
-            <li>Painting - <strong>Trix Mix</strong></li>
-            <li>Letters on Floor - <strong>faerieko</strong></li>
-            <li>Balloon Sets - <strong>0btuse</strong></li>
-            <li>Kronii Dakimakura, Pillows - <strong>sprub</strong></li>
-            <li>Bananas, Berries - <strong>Mogu Mogu</strong></li>
-            <li>Ceiling Flags - <strong>Mogu Mogu</strong></li>
-            <li>Lantern - <strong>oCat</strong></li>
-            <li>Spider - <strong>Error 404 Name Not Found</strong></li>
-            <li>Rice - <strong>oCat</strong></li>
-            <li>Shelf Items - <strong>AlphaCas</strong></li>
-            <li>Friend (all variants) - <strong>Eli</strong></li>
-            <li>Animol - <strong>Đay</strong></li>
-            <li>Music Player - <strong>Wherefore</strong></li>
+            <li>Birthday Cake - <strong>Faerieko</strong></li>
+            <li>Central Table - <strong>Nin-tan【人単】</strong></li>
+            <li>Birthday Banner - <strong>Pet</strong></li>
+            <li>Animal - <strong>AlphaCas</strong></li>
+            <li>Hoomans - <strong>Sayowl</strong></li>
+            <li>Friend - <strong>Chlorine Cocktail</strong></li>
+            <li>Hootsie - <strong>0btuse</strong></li>
+            <li>Letter Pile - <strong>DeezNiceDrawins</strong></li>
+            <li>Photos by the bed - <strong>Sayowl</strong></li>
+            <li>PC - <strong>Sayowl</strong></li>
+            <li>Framed Owl Adoption Certificate - <strong>Sayowl</strong></li>
+            <li>Pile of drawings - <strong>AlphaCas</strong></li>
+            <li>Floor Balloons, Floating Present - <strong>Sayowl</strong></li>
+            <li>Balloons by bed - <strong>AlphaCas</strong></li>
+            <li>
+              Birthday streamers, Figurines, Baseball Bat, Presents -
+              <strong>Chlorine Cocktail</strong>
+            </li>
+            <li>Sanalite - <strong>Nofuukee</strong></li>
           </ul>
         </li>
         <li>
           Music &amp; Sounds
           <ul>
             <li>Main BGM - <strong>Wherefore</strong></li>
-            <li>Lights Out BGM - <strong>Wherefore</strong></li>
-            <li>New Start Remix - <strong>Wherefore</strong></li>
             <li>Barks - <strong>Animol</strong></li>
           </ul>
         </li>
@@ -77,38 +75,6 @@
             <li>Quests Icon - <strong>jetrico</strong></li>
             <li>BGM Icon - <strong>Wherefore</strong></li>
           </ul>
-        </li>
-      </ul>
-
-      <h3 class="brown--text">Friend Project: World Tour</h3>
-      <ul class="pb-4">
-        <li>Organized by <strong>Mumei's Sanctuary Discord Staff</strong></li>
-      </ul>
-
-      <h3 class="brown--text">Special Thanks to:</h3>
-      <ul class="pb-4">
-        <li>Staff of Mumei Civilization and Sanctuary discords</li>
-      </ul>
-
-      <h3 class="brown--text">Links</h3>
-      <ul class="pb-4">
-        <li>
-          Source Code:
-          <a href="https://github.com/fromyourfans/mumei-birthday-2022/" target="_blank">
-            https://github.com/fromyourfans/mumei-birthday-2022/
-          </a>
-        </li>
-        <li>
-          Forked from
-          <a href="https://birthday.ennaalouette.com/" target="_blank">
-            https://birthday.ennaalouette.com
-          </a>
-        </li>
-        <li>
-          Drawing Board (closed):
-          <a href="https://aggie.io/gor2k0re89" target="_blank">
-            https://aggie.io/gor2k0re89
-          </a>
         </li>
       </ul>
     </div>
