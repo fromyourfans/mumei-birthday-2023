53c32fbb75a3a109b3a11739a5e4f3baed337730
jetrico <jetriconew@gmail.com>
2023-08-03 17:50:11 +0800

    2023: certificate project

--- src/components/ProjectCertificate.vue
+++ src/components/ProjectCertificate.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="project">
     <div class="project-description">
-      <h3>PlaceholderTitle</h3>
-      <p>PlaceholderDescription</p>
+      <h3>The Owls Trust Fundraiser Certificate</h3>
+      <p>The Owls Trust official fundraiser certificate.</p>
     </div>
     <div class="project-close">
       <v-btn
@@ -13,35 +13,26 @@
     </div>
     <div class="project-content" ref="container">
       <v-carousel height="100%" hide-delimiters progress>
-        <v-carousel-item :src="Cookbook01" contain ></v-carousel-item>
-        <v-carousel-item :src="Cookbook02" contain ></v-carousel-item>
-        <v-carousel-item :src="Cookbook03" contain ></v-carousel-item>
-        <v-carousel-item :src="Cookbook04" contain ></v-carousel-item>
-        <v-carousel-item :src="Cookbook05" contain ></v-carousel-item>
-        <v-carousel-item :src="Cookbook06" contain ></v-carousel-item>
+        <v-carousel-item :src="Cert1Img" contain ></v-carousel-item>
+        <v-carousel-item :src="Cert2Img" contain ></v-carousel-item>
+        <v-carousel-item :src="Cert3Img" contain ></v-carousel-item>
       </v-carousel>
     </div>
   </div>
 </template>
 
 <script>
-import Cookbook01 from '@/assets/mural-20220719.png';
-import Cookbook02 from '@/assets/mural-20220719.png';
-import Cookbook03 from '@/assets/mural-20220719.png';
-import Cookbook04 from '@/assets/mural-20220719.png';
-import Cookbook05 from '@/assets/mural-20220719.png';
-import Cookbook06 from '@/assets/mural-20220719.png';
+import Cert1Img from '@/assets/certs/cert1.png';
+import Cert2Img from '@/assets/certs/cert2.png';
+import Cert3Img from '@/assets/certs/cert3.png';
 
 export default {
   props: ['image'],
   data: () => ({
     maxHeight: 480,
-    Cookbook01,
-    Cookbook02,
-    Cookbook03,
-    Cookbook04,
-    Cookbook05,
-    Cookbook06,
+    Cert1Img,
+    Cert2Img,
+    Cert3Img,
   }),
   methods: {
     onImgLoaded() {
